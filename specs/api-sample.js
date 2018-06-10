bb447e991996878bde73b4ecadf24f0416ac322d
nimrh1k <nimrh1k@gmail.com>
2018-06-10 10:09:30 +0900

    no message

---
+++ specs/api-sample.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Sample API suit', () => {
+    it('should be ok', () => {
+        const expect = {
+            id: 1,
+            value: 'tempValue'
+        };
+        assert.deepEqual(expect, false);
+    })
+})
