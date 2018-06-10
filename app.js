eaa6bf995a2b92adc7bdfaaae04c50cc7548dd3a
hklee <dev.hklee@gmail.com>
2018-06-10 11:52:24 +0900

    no message

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
-
-
 const routes = require('./routes');
 
-
-
 routes.initRouters(app);
-
-
 
 app.listen(10000, () => {
     console.log('Koa is running!!');
 });
+
+module.exports = app;
--- specs/api-sample.js
+++ specs/api-sample.js
@@ -1,11 +1,27 @@
 const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+require('../app');
 
 describe('Sample API suit', () => {
-    it('should be ok', () => {
-        const expect = {
+    it('should be ok', done => {
+        const expectBody = {
             id: 1,
             value: 'tempValue'
         };
-        assert.deepEqual(expect, false);
+
+        const url = 'http://localhost:10000';
+        chai.request(url)
+            .get('/api/sample/v1/5')
+            .end(((err, res) => {
+
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                assert.equal(res.body.id, expectBody.id);
+                done();
+            }));
     })
 })
